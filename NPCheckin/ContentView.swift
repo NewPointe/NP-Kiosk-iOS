1cff4c28a34b6a8015992d0810cac4afdbc2158f
Tyler Schrock <tschrock123@gmail.com>
2020-04-27 06:33:48 -0400

    Initial Commit

---
+++ NPCheckin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NPCheckin
+//
+//  Created by Tyler Schrock on 4/27/20.
+//  Copyright © 2020 NewPointe Community Church. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
